c0a99eb4b0b67842e134f7eaf80318818f6f611c
s1f10210254 <s1f10210248@iniad.org>
2023-11-24 20:56:11 +0900

    login完成

--- client/src/pages/login/index.page.tsx
+++ client/src/pages/login/index.page.tsx
@@ -1,15 +1,24 @@
 import { APP_TITLE } from 'commonConstantsWithClient';
-import { GithubIcon } from 'src/components/icons/GithubIcon';
-import { loginWithGitHub } from 'src/utils/login';
+import { useState } from 'react';
+import { authWithEmail } from 'src/utils/login';
 import { useLoading } from '../@hooks/useLoading';
 import styles from './index.module.css';
 
 const Login = () => {
   const { loadingElm, addLoading, removeLoading } = useLoading();
 
-  const login = async () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // const loginGithub = async () => {
+  //   addLoading();
+  //   await loginWithGitHub();
+  //   removeLoading();
+  // };
+
+  const loginEmail = async () => {
     addLoading();
-    await loginWithGitHub();
+    await authWithEmail(email, password);
     removeLoading();
   };
 
@@ -17,10 +26,27 @@ const Login = () => {
     <div className={styles.container}>
       <div className={styles.main}>
         <div className={styles.title}>{APP_TITLE}</div>
-        <div style={{ marginTop: '16px' }} onClick={login}>
+        {/* <div style={{ marginTop: '16px' }} onClick={login}>
           <div className={styles.btn}>
             <GithubIcon size={18} fill="#fff" />
             <span>Login with GitHub</span>
+          </div>
+        </div> */}
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <div style={{ marginTop: '16px' }} onClick={loginEmail}>
+          <div className={styles.btn}>
+            <span>Login with Email</span>
           </div>
         </div>
       </div>
