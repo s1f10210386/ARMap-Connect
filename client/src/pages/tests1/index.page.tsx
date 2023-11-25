4b7fd21fb69b0e002835b55928404fcba11be066
s1f10210254 <s1f10210248@iniad.org>
2023-11-26 03:20:02 +0900

    muriaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- client/src/pages/tests1/index.page.tsx
+++ client/src/pages/tests1/index.page.tsx
@@ -1,31 +1,37 @@
 import { useAtom } from 'jotai';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { userAtom } from 'src/atoms/user';
 import { Loading } from 'src/components/Loading/Loading';
 import * as THREE from 'three';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import styles from './index.module.css';
 
 const Test1 = () => {
   const [user] = useAtom(userAtom);
-  const [flag, setFlag] = useState(false);
+  const [isCavasReady, setIsCavasReady] = useState(false);
 
   const videoRef = useRef<HTMLVideoElement>(null);
+  // const canvasRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLDivElement>(null);
-
+  useEffect(() => {
+    setTimeout(() => {
+      if (canvasRef.current !== null && canvasRef.current !== undefined) {
+        setIsCavasReady(true);
+      }
+    }, 0);
+  }, []);
+  console.log(isCavasReady);
   const text = '!!!!!!!!!!!!';
 
   useEffect(() => {
-    console.log('useEffect呼ばれる');
-    if (canvasRef !== null) {
-      console.log(canvasRef);
-      setFlag(true);
-    }
-  }, []);
-
-  const videoAR = useCallback(() => {
-    if (flag === true) {
+    // if (!canvasRef.current) {
+    //   console.log('canvasRefはまだ利用可能ではありません。');
+    //   return;
+    // }
+    if (isCavasReady && canvasRef.current !== null) {
+      console.log('canvasRef読み込まれた');
+      // カメラフィードの取得
       navigator.mediaDevices
         .getUserMedia({ video: true })
         .then((stream) => {
@@ -36,92 +42,74 @@ const Test1 = () => {
         .catch((err) => {
           console.error('カメラへのアクセスに失敗しました: ', err);
         });
-      console.log('a', canvasRef);
 
-      if (canvasRef.current) {
-        console.log('1', canvasRef);
-        const scene = new THREE.Scene();
+      const scene = new THREE.Scene();
+      const containerWidth = canvasRef.current.clientWidth;
+      const containerHeight = canvasRef.current.clientHeight;
+      const aspectRatio = containerWidth / containerHeight;
 
-        // フォントローダーを使用してテキストメッシュを作成
+      const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+      camera.position.z = 1.8;
 
-        const loader = new FontLoader();
-        const containerWidth = canvasRef.current.clientWidth;
-        const containerHeight = canvasRef.current.clientHeight;
-        const aspectRatio = containerWidth / containerHeight;
+      const renderer = new THREE.WebGLRenderer({ alpha: true });
+      renderer.setSize(containerWidth, containerHeight);
+      canvasRef.current.appendChild(renderer.domElement);
 
-        const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
-        camera.position.z = 1.8;
-
-        const renderer = new THREE.WebGLRenderer({ alpha: true });
-        renderer.setSize(containerWidth, containerHeight);
-
-        const currentContainer = canvasRef.current;
-        currentContainer.appendChild(renderer.domElement);
-        console.log('3');
-        loader.load('/fonts/helvetiker_regular.typeface.json', function (fonts) {
-          console.log('loader');
-          const textGeometry = new TextGeometry(text, {
-            font: fonts,
-            size: 0.2,
-            height: 0.02,
-            curveSegments: 10,
-            bevelEnabled: true,
-            bevelThickness: 0.01,
-            bevelSize: 0.01,
-            bevelSegments: 1,
-          });
-          const textMaterial = new THREE.MeshBasicMaterial({ color: 0xfff8f0 });
-          const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-
-          // テキストメッシュの位置設定
-          const fontSize = 0.2;
-          const averageCharWidth = fontSize * 0.68;
-          const textLength = text.length * averageCharWidth;
-          textMesh.position.set(-textLength, 0, 0);
-
-          scene.add(textMesh);
-
-          // アニメーションループ
-          let scaleDelta = 0.005;
-          const animate = () => {
-            // console.log(textMesh);
-            requestAnimationFrame(animate);
-            textMesh.scale.z += scaleDelta;
-            if (textMesh.scale.z > 1 || textMesh.scale.z < 0.1) {
-              scaleDelta *= -1;
-            }
-            renderer.render(scene, camera);
-          };
-          animate();
+      const loader = new FontLoader();
+      loader.load('/fonts/helvetiker_regular.typeface.json', function (fonts) {
+        const textGeometry = new TextGeometry(text, {
+          font: fonts,
+          size: 0.2,
+          height: 0.02,
+          curveSegments: 10,
+          bevelEnabled: true,
+          bevelThickness: 0.01,
+          bevelSize: 0.01,
+          bevelSegments: 1,
         });
-      }
+
+        const textMaterial = new THREE.MeshBasicMaterial({ color: 0xfff8f0 });
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+        const fontSize = 0.2;
+        const averageCharWidth = fontSize * 0.68;
+        const textLength = text.length * averageCharWidth;
+        textMesh.position.set(-textLength, 0, 0);
+
+        scene.add(textMesh);
+
+        let scaleDelta = 0.005;
+        const animate = () => {
+          requestAnimationFrame(animate);
+          textMesh.scale.z += scaleDelta;
+          if (textMesh.scale.z > 1 || textMesh.scale.z < 0.1) {
+            scaleDelta *= -1;
+          }
+          renderer.render(scene, camera);
+        };
+        animate();
+      });
     }
-  }, [flag]);
+  }, [isCavasReady]);
 
-  useEffect(() => {
-    videoAR();
-  }, [videoAR]);
-  // useEffect(() => {
-  //   // カメラフィードの取得
-  //   navigator.mediaDevices
-  //     .getUserMedia({ video: true })
-  //     .then((stream) => {
-  //       if (videoRef.current) {
-  //         videoRef.current.srcObject = stream;
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.error('カメラへのアクセスに失敗しました: ', err);
-  //     });
-  //   console.log('a', canvasRef);
-
+  // useLayoutEffect(() => {
+  //   console.log('cavasRef.current', canvasRef.current);
   //   if (canvasRef.current) {
-  //     console.log('1', canvasRef);
+  //     console.log('canvasRef読み込まれた');
+  //     // ここでカメラフィードの取得とその他の処理を実行
+
+  //     navigator.mediaDevices
+  //       .getUserMedia({ video: true })
+  //       .then((stream) => {
+  //         if (videoRef.current) {
+  //           videoRef.current.srcObject = stream;
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.error('カメラへのアクセスに失敗しました: ', err);
+  //       });
+
   //     const scene = new THREE.Scene();
-
-  //     // フォントローダーを使用してテキストメッシュを作成
-
-  //     const loader = new FontLoader();
   //     const containerWidth = canvasRef.current.clientWidth;
   //     const containerHeight = canvasRef.current.clientHeight;
   //     const aspectRatio = containerWidth / containerHeight;
@@ -131,12 +119,10 @@ const Test1 = () => {
 
   //     const renderer = new THREE.WebGLRenderer({ alpha: true });
   //     renderer.setSize(containerWidth, containerHeight);
+  //     canvasRef.current.appendChild(renderer.domElement);
 
-  //     const currentContainer = canvasRef.current;
-  //     currentContainer.appendChild(renderer.domElement);
-  //     console.log('3');
+  //     const loader = new FontLoader();
   //     loader.load('/fonts/helvetiker_regular.typeface.json', function (fonts) {
-  //       console.log('loader');
   //       const textGeometry = new TextGeometry(text, {
   //         font: fonts,
   //         size: 0.2,
@@ -147,10 +133,10 @@ const Test1 = () => {
   //         bevelSize: 0.01,
   //         bevelSegments: 1,
   //       });
+
   //       const textMaterial = new THREE.MeshBasicMaterial({ color: 0xfff8f0 });
   //       const textMesh = new THREE.Mesh(textGeometry, textMaterial);
 
-  //       // テキストメッシュの位置設定
   //       const fontSize = 0.2;
   //       const averageCharWidth = fontSize * 0.68;
   //       const textLength = text.length * averageCharWidth;
@@ -158,10 +144,8 @@ const Test1 = () => {
 
   //       scene.add(textMesh);
 
-  //       // アニメーションループ
   //       let scaleDelta = 0.005;
   //       const animate = () => {
-  //         // console.log(textMesh);
   //         requestAnimationFrame(animate);
   //         textMesh.scale.z += scaleDelta;
   //         if (textMesh.scale.z > 1 || textMesh.scale.z < 0.1) {
@@ -171,8 +155,10 @@ const Test1 = () => {
   //       };
   //       animate();
   //     });
+  //   } else {
+  //     console.log('canvasRefはまだ利用可能ではありません。');
   //   }
-  // }, [test]);
+  // }, []);
 
   if (!user) return <Loading visible />;
 
