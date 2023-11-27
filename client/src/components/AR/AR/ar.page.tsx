d98bbb93eeaafb3af4c477f45889f773a73bec20
s1f10210254 <s1f10210248@iniad.org>
2023-11-27 18:57:04 +0900

    DB連携

--- client/src/components/AR/AR/ar.page.tsx
+++ client/src/components/AR/AR/ar.page.tsx
@@ -1,7 +1,9 @@
 import { useAtom } from 'jotai';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { coordinatesAtom } from 'src/atoms/user';
+import { apiClient } from 'src/utils/apiClient';
+import { returnNull } from 'src/utils/returnNull';
 import styles from './ar.module.css';
 
 const ARComponent = () => {
@@ -22,15 +24,41 @@ const ARComponent = () => {
     }
   }, [setCoordinates]);
   console.log('coodinates', coordinates);
+  const [posts, setPosts] = useState<
+    | {
+        id: string;
+        userName: string;
+        postTime: string;
+        content: string;
+        latitude: number;
+        longitude: number;
+        userID: string;
+        likeCount: number;
+      }[]
+    | null
+  >(null);
+
+  const getPosts = useCallback(async () => {
+    if (coordinates.latitude === null || coordinates.longitude === null) return;
+    const latitude = coordinates.latitude;
+    const longitude = coordinates.longitude;
+    const data = await apiClient.posts.$get({ query: { latitude, longitude } }).catch(returnNull);
+    setPosts(data);
+    console.log('getPosts');
+  }, [coordinates.latitude, coordinates.longitude]);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
 
   // const [posts, setPosts] = useState(null);
 
-  const posts = [
-    { latitude: 35.6895, longitude: 139.6917, content: '東京タワー' },
-    { latitude: 35.779373072610795, longitude: 139.72486851576315, content: 'ひろき' },
-    // { latitude: 35.779373072610795, longitude: 139.72486851576315, content: 'bdddd' },
-    { latitude: 35.7780781399212, longitude: 139.72516114049802, content: 'bbbbb' },
-  ];
+  // const posts = [
+  //   { latitude: 35.6895, longitude: 139.6917, content: '東京タワー' },
+  //   { latitude: 35.779373072610795, longitude: 139.72486851576315, content: 'ひろき' },
+  //   // { latitude: 35.779373072610795, longitude: 139.72486851576315, content: 'bdddd' },
+  //   { latitude: 35.7780781399212, longitude: 139.72516114049802, content: 'bbbbb' },
+  // ];
 
   return (
     <div>
@@ -50,7 +78,7 @@ const ARComponent = () => {
           gps-new-entity-place="latitude:  35.779373072610795; longitude:139.72486851576315"
           scale="10 10 10"
         /> */}
-        {posts.map((post, index) => (
+        {/* {posts.map((post, index) => (
           <a-text
             key={index}
             value={post.content}
@@ -62,13 +90,22 @@ const ARComponent = () => {
             font="/fonts/noto-sans-cjk-jp-msdf.json"
             font-image="/png/noto-sans-cjk-jp-msdf.png"
           />
+        ))} */}
+
+        {posts?.map((post, index) => (
+          <a-text
+            key={index}
+            value={post.content}
+            gps-new-entity-place={`latitude: ${post.latitude}; longitude: ${post.longitude}`}
+            scale="10 10 10"
+          />
         ))}
         {coordinates.latitude !== null && coordinates.longitude !== null && (
           <a-text
             value={`Latitude: ${coordinates.latitude}, Longitude:${coordinates.longitude}`}
             position="5 -2 -5"
             scale="1 1 0"
-            color="#f9f7f7"
+            color="#000000"
             look-at="#camera"
             align="right"
             // font="/fonts/noto-sans-cjk-jp-msdf.json"
