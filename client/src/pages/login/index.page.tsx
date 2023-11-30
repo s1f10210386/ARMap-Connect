1e660d3157f362fff10f16ca2c8d6f418a77ac93
s1f10210254 <s1f10210248@iniad.org>
2023-11-30 13:49:09 +0900

    修正

--- client/src/pages/login/index.page.tsx
+++ client/src/pages/login/index.page.tsx
@@ -91,6 +91,13 @@ const Login = () => {
             {APP_TITLE}
           </Typography>
           {dev ? (
+            <div style={{ marginTop: '16px' }} onClick={loginGithub}>
+              <div className={styles.btn}>
+                <GithubIcon size={18} fill="#fff" />
+                <span>GitHubでのログインはこちら</span>
+              </div>
+            </div>
+          ) : (
             <div>
               <TextField
                 label="Email"
@@ -118,13 +125,6 @@ const Login = () => {
                   {loginError}
                 </Typography>
               )}
-            </div>
-          ) : (
-            <div style={{ marginTop: '16px' }} onClick={loginGithub}>
-              <div className={styles.btn}>
-                <GithubIcon size={18} fill="#fff" />
-                <span>GitHubでのログインはこちら</span>
-              </div>
             </div>
           )}
         </Box>
