c932311f408c930e289d93a524fecd548eba77d7
s1f10210254 <s1f10210248@iniad.org>
2023-12-02 02:38:41 +0900

    a

--- client/src/components/AR/AR/ar.page.tsx
+++ client/src/components/AR/AR/ar.page.tsx
@@ -218,7 +218,7 @@ const ARComponent = () => {
         ))}
 
         {/* ユーザーが５メートル以上移動した場合のみカメラの位置が更新 */}
-        <a-camera gps-camera="maxDistance:50" rotation-reader />
+        <a-camera gps-camera rotation-reader />
 
         <a-entity id="mouseCursor" cursor="rayOrigin: mouse" raycaster="objects: .raycastable" />
       </a-scene>
